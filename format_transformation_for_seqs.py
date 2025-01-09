a40789aed5cfce61cffec127474201aaefc47405
chrispersico <111577401+chrispersico@users.noreply.github.com>
2025-01-09 16:22:52 +0100

    Update format_transformation_for_seqs.py

--- format_transformation_for_seqs.py
+++ format_transformation_for_seqs.py
@@ -4,7 +4,7 @@ def convert_sequence_format(input_text):
     Input format:
     ID \n Taxonomy \n Sequence \n
     Output format:
-    >ID \n Fixed-width sequence with dashes for padding
+    >ID \n Sequence in uppercase
     """
     lines = input_text.strip().split('\n')
     output = []
@@ -15,19 +15,12 @@ def convert_sequence_format(input_text):
             break
             
         seq_id = lines[i].strip()
-        # taxo = lines[i + 1].strip()
-        sequence = lines[i + 2].strip()
+        sequence = lines[i + 2].strip().upper()
         
         output.append(f">{seq_id}")
-        
-        block_size = 150
-        sequence_blocks = []
-        for j in range(0, len(sequence), block_size):
-            block = sequence[j:j + block_size]
-            padded_block = block.ljust(block_size, '-')
-            sequence_blocks.append(padded_block)
-        output.extend(sequence_blocks)
+        output.append(sequence)
         output.append('')
+    
     return '\n'.join(output)
 
 def main():
